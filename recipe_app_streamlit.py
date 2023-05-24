dfb38e3e384f5b490392c8ba53f0157673e0af13
Jeeeeee11 <109201161+Jeeeeee11@users.noreply.github.com>
2023-05-25 01:55:23 +0800

    Update recipe_app_streamlit.py

--- recipe_app_streamlit.py
+++ recipe_app_streamlit.py
@@ -1,123 +1,93 @@
-import streamlit as st
-from recipe_generation import generation_function
-from streamlit_ace import st_ace
-import requests
-import os
-from PIL import Image
-import json
+from transformers import AutoTokenizer, AutoModelForSeq2SeqLM
+from transformers import pipeline
+import tensorrt
 
+MODEL_NAME_OR_PATH = "flax-community/t5-recipe-generation"
+tokenizer = AutoTokenizer.from_pretrained(MODEL_NAME_OR_PATH, use_fast=True)
+model = AutoModelForSeq2SeqLM.from_pretrained(MODEL_NAME_OR_PATH)
 
-def search_recipe(title, serper_dev_key):
-    url = "https://google.serper.dev/images"
-    payload = json.dumps({"q": title})
-    headers = {
-        'X-API-KEY': serper_dev_key,
-        'Content-Type': 'application/json'
-    }
-    response = requests.request("POST", url, headers=headers, data=payload)
-    if response.status_code == 200:
-        data = response.json()
-        if "images" in data:
-            images = data["images"]
-            if images and isinstance(images, list) and "original" in images[0]:
-                image_url = images[0]["original"]
-                return image_url
-    return None
+prefix = "items: "
+# generation_kwargs = {
+#     "max_length": 512,
+#     "min_length": 64,
+#     "no_repeat_ngram_size": 3,
+#     "early_stopping": True,
+#     "num_beams": 10,
+#     "length_penalty": 1.5,
+# }
+generation_kwargs = {
+    "max_length": 512,
+    "min_length": 64,
+    "no_repeat_ngram_size": 3,
+    "do_sample": True,
+    "top_k": 60,
+    "top_p": 0.95,
+    "num_return_sequences": 3,  # Generate 3 unique sequences
+    "temperature": 0.8
+}
 
+special_tokens = tokenizer.all_special_tokens
+tokens_map = {
+    "<sep>": "--",
+    "<section>": "\n"
+}
 
-# Replace this with your Serper.dev API key
-serper_dev_key = "4e9bb51dde629184a62c67633138d311a9bef367"
+def skip_special_tokens(text, special_tokens):
+    for token in special_tokens:
+        text = text.replace(token, "")
 
+    return text
 
-# Custom CSS for styling
-st.markdown(
-    """
-<style>
-    .reportview-container .main .block-container {
-        padding-top: 2rem;
-        padding-bottom: 2rem;
-    }
-    h1 {
-        font-size: 4rem;
-    }
-    h2 {
-        font-size: 3rem;
-    }
-    .stButton>button {
-        background-color: #000000;
-        color: white;
-        font-weight: bold;
-        border-radius: 8px;
-        padding: 8px 15px;
-    }
-</style>
-""",
-    unsafe_allow_html=True,
-)
+def target_postprocessing(texts, special_tokens):
+    if not isinstance(texts, list):
+        texts = [texts]
+    
+    new_texts = []
+    for text in texts:
+        text = skip_special_tokens(text, special_tokens)
 
-st.title("Chef Transformer")
+        for k, v in tokens_map.items():
+            text = text.replace(k, v)
 
-# Intro text
-st.write(
-    "Welcome to the Chef Transformer! Enter a list of ingredients separated by commas, "
-    "and the Chef Transformer will generate recipes for you."
-)
+        new_texts.append(text)
 
-# Input text using streamlit_ace
-items = st_ace(
-    placeholder="Enter ingredients (e.g., chicken, brown rice, carrots, garlic, soy sauce, olive oil)",
-    language="markdown",
-    theme="xcode",
-    keybinding="sublime",
-    font_size=18,
-    height=150,
-)
+    return new_texts
 
-# Generate recipes button
-if st.button("Generate Recipes"):
-    if items:
-        items_list = [items]
-        generated_recipes = generation_function(items_list, num_recipes=3)
-        ingredients_list = [ingredient.strip() for ingredient in items.split(',')]
+def generation_function(texts, num_recipes=1):
+    _inputs = texts if isinstance(texts, list) else [texts]
+    inputs = [prefix + inp for inp in _inputs]
+    inputs = tokenizer(
+        inputs, 
+        max_length=256, 
+        padding="max_length", 
+        truncation=True, 
+        return_tensors="pt"
+    )
 
+    input_ids = inputs.input_ids
+    attention_mask = inputs.attention_mask
+
+    generated_recipes = []
+    while len(generated_recipes) < num_recipes:
+        output_ids = model.generate(
+            input_ids=input_ids, 
+            attention_mask=attention_mask,
+            **generation_kwargs
+        )
+        generated = output_ids.detach().cpu().numpy()
+        generated_recipe = target_postprocessing(
+            tokenizer.batch_decode(generated, skip_special_tokens=False),
+            special_tokens
+        )
+
+        # Check if generated_recipe is unique and contains only inputted ingredients
+        unique = True
         for recipe in generated_recipes:
-            text = recipe[0]
-            sections = text.split("\n")
-            for section in sections:
-                section = section.strip()
-                if section.startswith("title:"):
-                    section = section.replace("title:", "")
-                    dish_title = section.strip().capitalize()
-                    image_url = search_recipe(dish_title, serper_dev_key)
-                    # ... rest of the section logic
+            if generated_recipe == recipe or not all(ingredient in generated_recipe[0] for ingredient in texts[0].split(',')):
+                unique = False
+                break
 
-        if image_url:
-            st.image(image_url, caption=dish_title, width=256)
-            st.markdown(f"**Title: {dish_title}**")
-            for recipe in generated_recipes:
-                text = recipe[0]
-                sections = text.split("\n")
-                for section in sections:
-                    section = section.strip()
-                    if section.startswith("ingredients:"):
-                        section = section.replace("ingredients:", "")
-                        headline = "Ingredients"
-                    elif section.startswith("directions:"):
-                        section = section.replace("directions:", "")
-                        headline = "Directions"
+        if unique:
+            generated_recipes.append(generated_recipe)
 
-                    if headline == "Ingredients":
-                        section_info = section.strip().capitalize()
-                        st.markdown(f"**{headline}:**", unsafe_allow_html=True)
-                        st.markdown(section_info, unsafe_allow_html=True)
-                    else:
-                        section_info = section.strip().capitalize()
-                        st.markdown(f"**{headline}:**", unsafe_allow_html=True)
-                        st.markdown(section_info, unsafe_allow_html=True)
-
-            st.write("-" * 130)
-
-    else:
-        st.warning("Please enter ingredients.")
-
-
+    return generated_recipes[0] if num_recipes == 1 else generated_recipes
